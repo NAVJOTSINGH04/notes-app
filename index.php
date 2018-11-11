13a32c483547f38f2654ee0c54f923a55ca14086
Navjot Singh <singhnavjot025@gmail.com>
2018-11-12 01:04:31 +0530

    landing page 1

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>Landing Page</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+      <link href="style.css" rel="stylesheet">
+
+ 
+  </head>
+  <body>
+<!--nav bar -->
+<nav class="navbar navbar-custom navbar-fixed">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Online Notes</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+          
+            <li><a href="#">Help </a></li>
+            <li><a href="#">Contact Us</a></li>
+        </ul>
+      
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Login</a></li>
+      
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<!--      jumbotron-->
+      <div class="jumbotron">
+  <h1>Online Notes App</h1>
+  <p>your Notes with you wherever you go.</p>
+          <p>Easy to use, protects all your notes!</p>
+  <p><a class="btn btn-primary btn-lg brown signup" href="#" role="button">Sign-up, Its-free</a></p>
+</div>
+   
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+   
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
